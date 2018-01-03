04609fd8e218362702c8303c70862cc99de3f2c5
guillaumeDaumas <dau.guil@gmail.com>
2018-01-03 17:04:24 +0100

    Client, Server et TestClient

--- src/server/Client.java
+++ src/server/Client.java
@@ -5,6 +5,27 @@ import utilisateurs.Utilisateur;
 import java.io.*;
 import java.net.*;
 
-public class Client {
+public class Client implements Runnable{
     Utilisateur courant;
+    BufferedWriter writer;
+    BufferedReader reader;
+
+    public Client(Utilisateur u) {
+        this.courant = u;
+        try {
+            Socket socketClient = new Socket("127.0.0.1",5555);
+
+            writer = new BufferedWriter(new OutputStreamWriter(socketClient.getOutputStream()));
+            reader = new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
+        }catch(Exception e){e.printStackTrace();}
+    }
+
+    public void run() {
+        try {
+            String serverMsg = "";
+            while((serverMsg = reader.readLine()) != null) {
+                System.out.println("depuis le serveur :" + serverMsg);
+            }
+        }catch(Exception e){e.printStackTrace();}
+    }
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -2,9 +2,11 @@ package server;
 
 import java.io.*;
 import java.net.*;
+import java.util.Vector;
 
 public class Server implements Runnable{
     Socket connectionSocket;
+    public static Vector clients = new Vector();
 
     public Server(Socket s) {
         try{
@@ -19,9 +21,20 @@ public class Server implements Runnable{
                     new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
             BufferedWriter writer =
                     new BufferedWriter(new OutputStreamWriter(connectionSocket.getOutputStream()));
+
+            clients.add(writer);
+
             while(true) {
                 String data1 = reader.readLine().trim();
                 System.out.println("Recu : " + data1);
+
+                for(int i=0;i<clients.size();i++) {
+                    try{
+                        BufferedWriter bw = (BufferedWriter)clients.get(i);
+                        bw.write(data1);
+                        bw.flush();
+                    }catch(Exception e){e.printStackTrace();}
+                }
             }
         }catch(Exception e){e.printStackTrace();}
     }
