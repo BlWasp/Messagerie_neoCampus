6345d246ca4c811bd23527f04ab4752548480fbe
guillaumeDaumas <dau.guil@gmail.com>
2018-01-15 19:40:09 +0100

    Fichier README et deux exit

--- src/discussion/FilDeDiscussion.java
+++ src/discussion/FilDeDiscussion.java
@@ -53,6 +53,7 @@ public class FilDeDiscussion implements Serializable,Comparable<FilDeDiscussion>
         }
         else{
             LOGGER.error("ERREUR : " + u.getPrenom() + " ne participe pas à cette conversation");
+            System.exit(1);
         }
         return messageajoute;
     }
